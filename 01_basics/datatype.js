03e5611a09a4f2da34e358f80ca7594aa4830b63
KanojiaVikas <138602192+KanojiaVikas@users.noreply.github.com>
2024-12-24 15:15:36 +0000

    datatypes

---
+++ 01_basics/datatype.js
@@ -0,0 +1,28 @@
+" us strict"; //treat all code as newer version
+
+//alert(3+3)  gives errror because we are using nodejs, not broswer
+
+console.log(3+3);
+console.log("viksas")  // code reusability should be high
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
+
